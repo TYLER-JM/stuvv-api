82f16e4f01ab544ad13197a5522422abc3d79560
tyler martin <t.mar92@gmail.com>
2019-11-25 13:46:31 -0500

    makes cookie setting work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
   # before_action :set_csrf_cookie
 
   # include ActionController::Cookies
   # include ActionController::RequestForgeryProtection
 
   # protect_from_forgery with: :exception
+
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :exception,  unless: -> { request.format.json? }
+  skip_before_action :verify_authenticity_token
 
   # private
 
