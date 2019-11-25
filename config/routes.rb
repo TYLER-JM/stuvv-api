39d96a2ef1e83ce22d47c32bad794a2fe9c39f03
Tamires <tamires.lowande@gmail.com>
2019-11-25 14:20:46 -0500

    adds the search-bar logic to the listings controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create] 
   get '/sessions' => 'sessions#destroy'
+  post '/search' => 'listings#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
