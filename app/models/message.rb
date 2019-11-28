17d01e1005b50923de87133c2b24a9772b76d67c
Tamires <tamires.lowande@gmail.com>
2019-11-28 13:15:57 -0500

    adds user information to the jbuilder object

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,10 @@ class Message < ApplicationRecord
   belongs_to :to_user, :class_name => 'User'
   belongs_to :from_user, :class_name => 'User'
 
-  #added
-  # belongs_to :listing
+  def name
+    "Mikias"
+  end
+  
   has_one :request
   has_one :listing, through: :request
 end
