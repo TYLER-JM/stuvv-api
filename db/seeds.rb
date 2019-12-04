dd0d013c669ef7debb9d263008669d9abd3a2437
Tamires <tamires.lowande@gmail.com>
2019-12-04 11:06:24 -0500

    changing seeds to remove faker for heroku seeds to run

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
+#require 'faker'
 require 'json'
 
 #5.times do
